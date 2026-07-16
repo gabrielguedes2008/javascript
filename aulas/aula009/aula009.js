4c22898f7c51548e2a5b675409bb99b2fc98904e
Gabriel Guedes <ygabrielgoff@gmail.com>
2026-07-16 17:14:40 -0300

    novos exercicios

---
+++ aulas/aula009/aula009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+pais = pais.toLocaleUpperCase
+
+if (pais = 'BRASIL') { // condição composta
+    console.log('Você é Brasileiro!!')
+} else {
+    console.log('Você é Estrangeiro!!')
+}
---
+++ aulas/aula009/index.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 50) { //condição simples
+    console.log('Você foi multado por excesso de velocidade')
+}
